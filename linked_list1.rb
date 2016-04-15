b8ed6f59765566724150bb91a2c68fb633f8c9d2
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-04-15 07:36:14 -0700

    initial commit

---
+++ linked_list1.rb
@@ -0,0 +1,20 @@
+class Stack
+    attr_reader :data
+    
+    def initialize
+        @data = nil
+    end
+
+    # Push a value onto the stack
+    def push(value)
+        # IMPLEMENT ME!
+    end
+
+    # Pop an item off the stack.  
+    # Remove the last item that was pushed onto the
+    # stack and return the value to the user
+    def pop
+        # I RETURN A VALUE
+    end
+
+end
